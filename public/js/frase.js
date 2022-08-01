b2197ba154633a5c28ba310048c4fc9f520ab6a9
Lara Simmer <larafsimmer@gmail.com>
2022-08-01 20:44:52 -0300

    Mudando frase aleatoriamente por meio de requisicao .get

---
+++ public/js/frase.js
@@ -0,0 +1,19 @@
+//VARIÁVEIS
+
+const botaoFrase = $("#botao-frase");
+
+//CHAMANDO FUNÇÕES
+
+botaoFrase.on("click", fraseAleatoria);
+
+//DECLARANDO FUNÇÕES
+
+function fraseAleatoria() {
+    $.get("http://localhost:3000/frases", trocaFraseAleatoriamente);
+}
+
+function trocaFraseAleatoriamente(data) {
+    let frase = $(".frase");
+    let numeroAleatorio = Math.floor(Math.random() * data.length)
+    frase.text(data[numeroAleatorio].texto);
+}
